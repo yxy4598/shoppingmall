9624fbad9987bd1609f5f3e6a83f69923bc3aad9
billowcloud <yu.life@qq.com>
2021-06-03 20:41:10 +0800

    配置文件别名和代码规范文件

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+// 配置 vue 文件的别名
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+      }
+    }
+  }
+}
