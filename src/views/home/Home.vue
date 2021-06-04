97b2ed031024212c352a58b5572913db8d85d3f3
billowcloud <yu.life@qq.com>
2021-06-04 22:44:42 +0800

    首页相关页面展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,63 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav">
-      <div slot="center">购物车</div>
+      <div slot="center">首页</div>
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"></tab-control>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+    <li>liuxing</li>
+
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
   import HomeSwiper from './childComs/HomeSwiper'
   import RecommendView from './childComs/RecommendView'
+  import FeatureView from './childComs/FeatureView'
+
+  import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from 'components/content/tabControl/TabControl'
 
   import {getHomeMultidata} from "network/home"
 
@@ -26,7 +72,9 @@
     components: {
       NavBar,
       HomeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView,
+      TabControl
     },
     created() {
       getHomeMultidata().then(res => {
@@ -39,8 +87,18 @@
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 1;
   }
 </style>
