ec8e3f91762d39b2915e2511e3332d632c401e8d
billowcloud <yu.life@qq.com>
2021-06-05 22:29:05 +0800

    引入并且封装第三方库better-scroll

--- src/components/common/tabbar/TabBar.vue
+++ src/components/common/tabbar/TabBar.vue
@@ -21,6 +21,7 @@
     bottom: 0;
 
     box-shadow: 0 -2px 1px rgba(100, 100, 100, .2);
+    z-index: 1;
   }
   
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,12 +3,20 @@
     <nav-bar class="home-nav">
       <div slot="center">首页</div>
     </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"></tab-control>
-    <goods-list :goods="goods['pop'].list"></goods-list>
-
+    <scroll class="content">
+      <home-swiper :banners="banners"></home-swiper>
+      
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="goods[currentClick].list"></goods-list>
+    </scroll>
+    <!-- <home-swiper :banners="banners"></home-swiper>
+      
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="goods[currentClick].list"></goods-list> -->
   </div>
 </template>
 
@@ -20,6 +28,7 @@
   import NavBar from 'components/common/navbar/NavBar'
   import TabControl from 'components/content/tabControl/TabControl'
   import GoodsList from 'components/content/goods/GoodsList'
+  import Scroll from 'components/common/scroll/Scroll'
 
   import {getHomeMultidata, getHomeGoods} from "network/home"
 
@@ -33,7 +42,8 @@
           'pop': {page: 0, list: []},
           'new': {page: 0, list: []},
           'sell': {page: 0, list: []}
-        }
+        },
+        currentClick: 'pop'
       }
     },
     components: {
@@ -42,7 +52,8 @@
       RecommendView,
       FeatureView,
       TabControl,
-      GoodsList
+      GoodsList,
+      Scroll
     },
     created() {
       this.getHomeMultidata(),
@@ -51,6 +62,27 @@
       this.getHomeGoods('sell')
     },
     methods: {
+      /**
+       * 事件的监听方法
+       */
+      tabClick(index) {
+        switch(index) {
+          case 0:
+            this.currentClick = 'pop';
+            break;
+          case 1:
+            this.currentClick = 'new';
+            break;
+          case 2:
+            this.currentClick = 'sell'
+            break;
+        }
+        // console.log(index);
+      },
+
+      /**
+       * 网络请求的方法 
+       */
       getHomeMultidata() {
         getHomeMultidata().then(res => {
           // console.log(res);
@@ -79,6 +111,8 @@
 <style scoped>
   #home {
     padding-top: 44px;
+    height: 100vh;
+    position: relative;
   }
 
   .home-nav {
@@ -97,5 +131,16 @@
     top: 44px;
 
     z-index: 1;
+  }
+
+  .content {
+    overflow: hidden;
+    position: absolute;
+
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+
   }
 </style>
