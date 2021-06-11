7caabe44a9c7cb260184b7a0bec288cd20faf9c3
billowcloud <yu.life@qq.com>
2021-06-12 00:07:16 +0800

    detail详情页信息

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,29 +1,41 @@
 <template>
-  <div>
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
-    <detail-base-info :goods="goods"></detail-base-info>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" :pullUpLoad="true" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
+    </scroll>
   </div>
 </template>
 
 <script>
   import DetailNavBar from './childComps/DetailNavBar.vue'
-  import {getDetail, Goods} from "network/detail";
+  import {getDetail, Goods, Shop} from "network/detail";
   import DetailSwiper from "./childComps/DetailSwiper";
   import DetailBaseInfo from "./childComps/DetailBaseInfo";
+  import DetailShopInfo from "./childComps/DetailShopInfo";
+  import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+  import Scroll from "components/common/scroll/Scroll";
 
   export default {
     name: 'Detail',
     components: {
       DetailNavBar,
       DetailSwiper,
-      DetailBaseInfo
+      DetailBaseInfo,
+      DetailShopInfo,
+      DetailGoodsInfo,
+      Scroll
     },
     data() {
       return {
         iid: null,
         topImages: [],
-        goods: {}
+        goods: {},
+        shop: {},
+        detailInfo: {}
       }
     },
     created() {
@@ -34,12 +46,33 @@
         console.log(res);
         const data = res.data.result
         this.topImages = data.itemInfo.topImages;
-        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services);
+        this.shop = new Shop(data.shopInfo);
+        this.detailInfo = data.detailInfo;
       })
+    },
+    methods: {
+      imageLoad() {
+        this.$refs.scroll.refresh()
+      }
     }
   }
 </script>
 
 <style scoped>
+  #detail {
+    position: relative;
+    z-index: 2;
+    background-color: #fff;
 
+    height: 100vh;
+  }
+  .detail-nav {
+    position: relative;
+    z-index: 1;
+    background-color: #fff;
+  }
+  .content{
+    height: calc(100% - 44px);
+  }
 </style>
