7a83388a4f6e2db319e1139978eabe16a871a082
billowcloud <yu.life@qq.com>
2021-06-13 09:55:10 +0800

    详情页数据展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -6,17 +6,19 @@
       <detail-base-info :goods="goods"></detail-base-info>
       <detail-shop-info :shop="shop"></detail-shop-info>
       <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
+      <detail-param-info :param-info="paramInfo"></detail-param-info>
     </scroll>
   </div>
 </template>
 
 <script>
   import DetailNavBar from './childComps/DetailNavBar.vue'
-  import {getDetail, Goods, Shop} from "network/detail";
+  import {getDetail, Goods, Shop, GoodsParam} from "network/detail";
   import DetailSwiper from "./childComps/DetailSwiper";
   import DetailBaseInfo from "./childComps/DetailBaseInfo";
   import DetailShopInfo from "./childComps/DetailShopInfo";
   import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+  import DetailParamInfo from "./childComps/DetailParamInfo";
   import Scroll from "components/common/scroll/Scroll";
 
   export default {
@@ -27,7 +29,8 @@
       DetailBaseInfo,
       DetailShopInfo,
       DetailGoodsInfo,
-      Scroll
+      Scroll,
+      DetailParamInfo
     },
     data() {
       return {
@@ -35,7 +38,8 @@
         topImages: [],
         goods: {},
         shop: {},
-        detailInfo: {}
+        detailInfo: {},
+        paramInfo: {}
       }
     },
     created() {
@@ -49,6 +53,7 @@
         this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services);
         this.shop = new Shop(data.shopInfo);
         this.detailInfo = data.detailInfo;
+        this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
       })
     },
     methods: {
